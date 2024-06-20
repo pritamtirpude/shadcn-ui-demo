78ce86227150e02b2c6a16d231ed9c0b0dabb44b
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2024-06-20 21:50:10 +0530

    charts integrated from employee stats

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import EmployeesStats from "@/components/employees-stats";
+import EmployeesStats from "@/components/employees/employees-stats";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function DashboardPage() {
   return (
-    <Tabs>
-      <TabsList defaultValue="employees" className="mb-4">
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
         <TabsTrigger value="employees">Employee Stats</TabsTrigger>
         <TabsTrigger value="teams">Teams Stats</TabsTrigger>
       </TabsList>
---
+++ components/employees/work-location-trends.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const data = [
+  {
+    name: "Jan",
+    office: 82,
+    wfh: 44,
+  },
+  {
+    name: "Feb",
+    office: 80,
+    wfh: 40,
+  },
+  {
+    name: "Mar",
+    office: 83,
+    wfh: 42,
+  },
+  {
+    name: "Apr",
+    office: 50,
+    wfh: 50,
+  },
+  {
+    name: "May",
+    office: 40,
+    wfh: 60,
+  },
+  {
+    name: "Jun",
+    office: 60,
+    wfh: 40,
+  },
+  {
+    name: "Jul",
+    office: 55,
+    wfh: 55,
+  },
+  {
+    name: "Aug",
+    office: 49,
+    wfh: 61,
+  },
+  {
+    name: "Sep",
+    office: 44,
+    wfh: 70,
+  },
+  {
+    name: "Oct",
+    office: 40,
+    wfh: 40,
+  },
+  {
+    name: "Nov",
+    office: 50,
+    wfh: 50,
+  },
+  {
+    name: "Dec",
+    office: 50,
+    wfh: 50,
+  },
+];
+
+export default function WorkLocationTrends() {
+  return (
+    <ResponsiveContainer height={350} width="100%">
+      <BarChart
+        data={data}
+        className="[&_.recharts-tooltip-cursor]:fill-zinc-200 dark:[&_.recharts-tooltip-cursor]:fill-zinc-800"
+      >
+        <XAxis dataKey="name" stroke="#888888" fontSize={12} />
+        <YAxis stroke="#888888" fontSize={12} />
+        <Tooltip
+          separator=": "
+          formatter={(value, name) => {
+            if (name === "office") {
+              return [value, "Work from office"];
+            } else if (name === "wfh") {
+              return [value, "Work from home"];
+            }
+          }}
+          labelClassName="font-bold"
+          wrapperClassName="!text-sm dark:!bg-black rounded-md dark:!border-border"
+        />
+        <Legend
+          iconType="circle"
+          formatter={(value) => {
+            if (value === "office") {
+              return <div>Work from office</div>;
+            } else if (value === "wfh") {
+              return <div>Work from home</div>;
+            }
+          }}
+        />
+        <Bar dataKey="office" stackId={1} fill="#ec4899" />
+        <Bar dataKey="wfh" stackId={1} fill="#6b7280" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
